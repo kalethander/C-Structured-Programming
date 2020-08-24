bd7435214d2bcdf385d1ccd4eebecd52f9b36dd7
kalethander <65253273+kalethander@users.noreply.github.com>
2020-08-24 19:03:30 -0400

    Add files via upload

--- honors_assignment.c
+++ honors_assignment.c
@@ -18,21 +18,21 @@ typedef struct int_list{
 } int_list;
 
 
-int_list* to_front( int_list* list)
+int_list* to_front( int_list* list)	// direct to list head
 {
     while( list -> prev != NULL)
 	list = list -> prev;
     return list;
 }
 
-int_list* to_back( int_list* list)
+int_list* to_back( int_list* list)	// direct to list tail
 {
     while( list -> next != NULL)
 	list = list -> next;
     return list;
 }
 
-void print_forward( int_list* list)
+void print_forward( int_list* list)	// print list in order
 {
     int i = 0;
     list = to_front( list);
@@ -42,21 +42,21 @@ void print_forward( int_list* list)
     }
 }
 
-int_list* rm_node( int_list* node)
+int_list* rm_node( int_list* node)	// remove a node
 {
     int_list* p1 = node -> prev;
     int_list* p2 = node -> next;
-    if (p1 == NULL) { p2 -> prev = NULL;}
-    else if (p2 == NULL) { p1 -> next = NULL;}
+    if (p1 == NULL) { p2 -> prev = NULL; return p2;}
+    else if (p2 == NULL) { p1 -> next = NULL; return p1;}
     else {
 	p1 -> next = p2;
 	p2 -> prev = p1;
+	free(node);
+	return p2;
     }
-    free(node);
-    return p2;
 }
 
-int_list* access_element( int_list* list, int n)
+int_list* access_element( int_list* list, int n)	// access element n
 {
     list = to_front( list);
     for( int i = 0; i < (n - 1); i++)
@@ -64,20 +64,22 @@ int_list* access_element( int_list* list, int n)
     return list;
 }
 
-int_list* rm_duplicates( int_list* list, int n)
+int_list* rm_duplicates( int_list* list, int n)		// remove integer duplicates
 {
     for( int i = 1; i < n; i++) 
     {
-	for( int j = i + 1; j < n; j++)
+	for( int j = i + 1; j <= n; j++)
 	{
 	    if( access_element( list, i)->d == access_element( list, j)->d) {
-		list = rm_node(access_element( list, j)); n--;}
+		list = rm_node(access_element( list, j)); 
+		n--;
+	    }
 	}
     }
     return list;
 }
 
-int_list* create_node( int data)
+int_list* create_node( int data)	// creates new node instance
 {
     int_list* node = malloc(sizeof(int_list));
     node -> d = data;
@@ -86,7 +88,7 @@ int_list* create_node( int data)
     return node;
 }
 
-int_list* add_to_front( int data, int_list* list)
+int_list* add_to_front( int data, int_list* list)	// append node to front of list
 {
     int_list* new_node = create_node( data);
     list = to_front( list);
@@ -95,7 +97,7 @@ int_list* add_to_front( int data, int_list* list)
     return new_node;
 }
 
-int_list* generate_list( int n)
+int_list* generate_list( int n)		// generates a list of nodes
 {
     int i;
     srand(clock());
@@ -116,71 +118,6 @@ int main(void)
     list = rm_duplicates( list, n);
     printf("\nMutated list: \n");
     print_forward( list); 
+
     return 0;
 }
-
-
-
-
-
-/*
-
-
-int_list* rm_duplicates( int_list* list)
-{
-    int* data;
-    int j = 0;
-    list = to_front( list);
-    while( list -> next != NULL) {
-	j++;
-	data = &(list -> d);
-	list = list -> next;
-	while( list -> next != NULL) {
-	    if( list -> d == *data)
-		list = rm_node( list);
-	    list = list -> next;    
-	}
-	list = to_front( list);
-	for( int i = 0; i < j; i++)
-	    list = list -> next;
-    }
-    list = to_front(list);
-    return list;
-}
-
-
-*/
-
-
-
-
-/*
-list = to_back(list);
-    if( (list -> prev) -> d == list -> d)
-	list = rm_node( list);
- */
-
-
-
-
-
-/* int_list* add_to_back( int data, int_list* list)
-{
-    int_list* new_node = create_node( data);
-    list = to_back( list);
-    list -> next = new_node;
-    new_node -> prev = list;
-    list = to_front(new_node);
-    return list;
-} */
-
-/*  
-void print_backward(int_list* list)
-{
-    int i = 0;
-    list = to_back( list);
-    while(list != NULL) {
-	printf("Element %d is %d\n", i++, list -> d);
-	list = list -> prev;
-    }
-} */
